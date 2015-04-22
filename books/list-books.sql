bff94baef3393d1930f959dd78c0da6b06e25f6a
Edouard Lopez <dev+git@edouard-lopez.com>
2015-04-22 20:50:12 +0200

    fix(sql): replace INNER by LEFT OUTER JOIN

--- books/list-books.sql
+++ books/list-books.sql
@@ -14,14 +14,14 @@ serie.id AS series,
 --   book.id   AS bid
 FROM books AS book
   -- fetch author data
-  INNER JOIN books_authors_link ON books_authors_link.book = book.id 
-  INNER JOIN authors AS author ON books_authors_link.author = author.id
+  LEFT OUTER JOIN books_authors_link ON books_authors_link.book = book.id
+  LEFT OUTER JOIN authors AS author ON books_authors_link.author = author.id
     -- fetch comments data
-  INNER JOIN comments ON comments.book = book.id
+  LEFT OUTER JOIN comments ON comments.book = book.id
   -- fetch tags data
-  INNER JOIN books_tags_link ON books_tags_link.book = book.id
-  INNER JOIN tags AS tag ON books_tags_link.tag = tag.id 
+  LEFT OUTER JOIN books_tags_link ON books_tags_link.book = book.id
+  LEFT OUTER JOIN tags AS tag ON books_tags_link.tag = tag.id
   -- fetch serie data
-  INNER JOIN books_series_link ON books_series_link.book = book.id
-  INNER JOIN series AS serie ON books_series_link.series = serie.id 
+  LEFT OUTER JOIN books_series_link ON books_series_link.book = book.id
+  LEFT OUTER JOIN series AS serie ON books_series_link.series = serie.id
 ;
